a80a9537f9fe0b2e7cd3f9e14046dc8c18cf114f
Vikas Banjare <vikasbanjare6721@gmail.com>
2026-08-27 23:52:56 +0530

    Learning C++

---
+++ tutorials/helloworld.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+    std::cout << "i like mangos" << "\n";
+    std::cout << "And HELLO WORLD" << "\n";
+    return 0;
+}
---
+++ tutorials/variables.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    int x;
+    x = 5;
+    std::cout << x ;
+    return 0;
+}
