f2b93ea28886217e2527e02e268d19a4e4f37c33
Vikas Banjare <vikasbanjare6721@gmail.com>
2026-08-28 22:10:43 +0530

    Update Const_Keyword.cpp

--- tutorials/Const_Keyword.cpp
+++ tutorials/Const_Keyword.cpp
@@ -3,11 +3,13 @@
 int main(){
     //const keyword is used to declare a variable as constant, meaning its value cannot be changed after initialization.
     
-    const int x = 10; // x is a constant integer with a value of 10
-    const double pi = 3.14159; // pi is a constant double with a value of 3.14159
-    
-    std::cout << x << "\n";
-    std::cout << pi << "\n";
+    const double RADIUS = 2; // radius is a constant integer with a value of 10
+    const double PI = 3.14159; // Pi is a constant double with a value of 3.14159
+    double circumference = 2 * PI * RADIUS; // circumference is calculated using the constant values of pi and radius
+   
+    std::cout << RADIUS << "\n";
+    std::cout << PI << "\n";
+    std::cout << circumference << "\n";
 
     return 0;
 }
