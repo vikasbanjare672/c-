d876eb4a18c6aa68c65501e5170fbb9defde8834
Vikas Banjare <vikasbanjare6721@gmail.com>
2026-08-28 23:17:04 +0530

    Create Arithmetic_Operators.cpp

---
+++ tutorials/Arithmetic_Operators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// Arithmetic Operators = used to perform common mathematical operations
+
+int main() {
+    int x = 10;
+    int y = 5;
+
+    std::cout << "Addition: " << x + y << "\n"; // Output: 15
+    std::cout << "Subtraction: " << x - y << "\n"; // Output: 5
+    std::cout << "Multiplication: " << x * y << "\n"; // Output: 50
+    std::cout << "Division: " << x / y << "\n"; // Output: 2
+    std::cout << "Modulus: " << x % y << "\n"; // Output: 0
+
+    return 0;
+}
