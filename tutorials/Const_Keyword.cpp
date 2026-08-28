ecce532aeefcc188ad2ee063b1579441d384c699
Vikas Banjare <vikasbanjare6721@gmail.com>
2026-08-28 21:49:58 +0530

    Learning about setting constants

---
+++ tutorials/Const_Keyword.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    //const keyword is used to declare a variable as constant, meaning its value cannot be changed after initialization.
+    
+    const int x = 10; // x is a constant integer with a value of 10
+    const double pi = 3.14159; // pi is a constant double with a value of 3.14159
+    
+    std::cout << x << "\n";
+    std::cout << pi << "\n";
+
+    return 0;
+}
