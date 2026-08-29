a0b368f2af5b376e24c165f34a65e8ccda14105d
Vikas Banjare <vikasbanjare6721@gmail.com>
2026-08-29 10:13:36 +0530

    Create User_Input.cpp

---
+++ tutorials/User_Input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// Arithmetic Operators = used to perform common mathematical operations
+
+int main() {
+    int stu = 20;
+    std::cout << stu << "\n";
+
+    return 0;
+}
