02d7b0dddad7bad388cfe72f3e61ddc9278d92a1
Vikas Banjare <vikasbanjare6721@gmail.com>
2026-08-31 01:58:26 +0530

    Update Simple_Calculator.cpp

--- tutorials/Simple_Calculator.cpp
+++ tutorials/Simple_Calculator.cpp
@@ -6,7 +6,7 @@ int main(){
     char op;
     double num1 , num2;
     double result;
-
+    std::cout << "***********CALCULATOR***********\n";
     std::cout << "Enter an operator (+, -, *, /): ";
     std::cin >> op;
     
@@ -19,21 +19,22 @@ int main(){
     switch(op){
         case '+':
             result = num1 + num2;
-            std::cout << result;
+            std::cout << "Result: " << result << "\n";
             break;
         case '-':   
             result = num1 - num2;
-            std::cout << result;
+            std::cout << "Result: " << result << "\n";
             break;
         case '*':
             result = num1 * num2;
-            std::cout << result;
+            std::cout << "Result: " << result << "\n";
             break;
         case '/':
             result = num1 / num2;
-            std::cout << result;
+            std::cout << "Result: " << result << "\n";
             break;
         default:
             std::cout << "Invalid operator!";
     }
+    std::cout << "********************************\n";
 }
