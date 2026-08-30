12e1ccce32ccfe762b2607fa52b2d1aa9f6e6f2d
Vikas Banjare <vikasbanjare6721@gmail.com>
2026-08-31 00:13:46 +0530

    Update User_Input.cpp

--- tutorials/User_Input.cpp
+++ tutorials/User_Input.cpp
@@ -4,11 +4,18 @@
 
 int main() {
     std::string name ;
-   
-    std::cout << "What's your name? ";
-    std::cin >> name; // Taking user input and storing it in the variable 'name'
-   
+    int age;
+
+    std::cout << "How old are you? ";
+    std::cin >> age; // Taking user input and storing it in the variable 'age'
+    
+    std::cout << "What's your full name? ";
+    std::getline(std::cin >> std::ws , name); // Taking user input and storing it in the variable 'name'   
+
+   //getline function is used to read a line of text from the input stream, including spaces.
+   //The std::ws manipulator is used to consume any leading whitespace characters before reading the input.    
+
     std::cout << "Hello," << name << "!\n"; 
-   
+    std::cout << "You are " << age << " years old.\n";
     return 0;
 }
