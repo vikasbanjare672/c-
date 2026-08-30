86faa84773ee0d413e2d6b8334388a61f6ed10f3
Vikas Banjare <vikasbanjare6721@gmail.com>
2026-08-30 23:37:03 +0530

    Update User_Input.cpp

--- tutorials/User_Input.cpp
+++ tutorials/User_Input.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
-// Arithmetic Operators = used to perform common mathematical operations
+// This program demonstrates how to take user input in C++ using the std::cin object.
 
 int main() {
-    int stu = 20;
-    std::cout << stu << "\n";
-
+    std::string name ;
+   
+    std::cout << "What's your name? ";
+    std::cin >> name; // Taking user input and storing it in the variable 'name'
+   
+    std::cout << "Hello," << name << "!\n"; 
+   
     return 0;
 }
