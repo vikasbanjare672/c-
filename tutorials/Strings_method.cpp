0ee520714d3ba61f23fd853f2762d865a686c46e
Vikas Banjare <vikasbanjare6721@gmail.com>
2026-08-31 22:42:05 +0530

    Update Strings_method.cpp

--- tutorials/Strings_method.cpp
+++ tutorials/Strings_method.cpp
@@ -6,15 +6,19 @@ int main(){
     std::cout << "Enter your name: ";
     std::getline(std::cin, name);
 
-    // Operation ::::::
+    // Operations ::::::
 
 /*  if (name.length() > 20) {
         std::cout << "Name is too long! Please enter a name with 20 characters or fewer.\n";
         return 1; // Exit the program with an error code
     }                                                              */
-    if (name.empty()) {
+/*  if (name.empty()) {
         std::cout << "Name cannot be empty! Please enter a valid name.\n";
-    }
+        }                                                           */
+//  name.append(" is a good person");
+//  name.insert(0, "Mr.");
+//  name.replace(0, 2, "Ms.");
+    name.erase(0, 2); // Erase the first two characters of the name
     std::cout << "Hello " << name << "\n" ;
     std::cout << "********************************\n";
     return 0;
