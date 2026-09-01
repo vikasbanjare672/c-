37e47f3c6b5cb8c619741e0ae0a51103f2dbd3d6
Vikas Banjare <vikasbanjare6721@gmail.com>
2026-09-01 15:30:11 +0530

    Create for_loop.cpp

---
+++ tutorials/for_loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    std::string name;
+    for(int i = 1; i <= 3 ; i++){
+        std::cout << "SYBAU!! and Give me your fucking info bitch!" << "\n";
+        std::cout << "Enter your name: ";
+        std::cin >> name;
+    }
+    return 0;
+}
