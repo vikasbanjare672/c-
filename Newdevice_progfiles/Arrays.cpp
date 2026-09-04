85302aaec807924218e50404dfb937ad24dba752
Vikas Banjare <vikasbanjare6721@gmail.com>
2026-09-05 02:01:53 +0530

    Create Arrays.cpp

---
+++ Newdevice_progfiles/Arrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+//  Array = a data structure that can hold multiple values 
+//          values are accessed by an index number
+//          "kind of like a variable that holds multiple values but all the values are have to be same data type"
+
+
+int main(){
+    
+    std::string laptop_bands[] = {"HP", "Lenovo", "dell"};
+    std::cout << laptop_bands[1];
+}
