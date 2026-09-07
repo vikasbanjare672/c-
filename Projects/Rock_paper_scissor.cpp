044ff68a22d4a189853e144f912c665a22241605
Vikas Banjare <vikasbanjare6721@gmail.com>
2026-09-08 00:54:33 +0530

    Update Rock_paper_scissor.cpp

--- Projects/Rock_paper_scissor.cpp
+++ Projects/Rock_paper_scissor.cpp
@@ -7,8 +7,9 @@ char getComputerChoice();
 void showchoice(char choice);
 void chooseWinner(char player, char computer);
 
-int main(){
-    
+int main()
+{
+
     char playerChoice, computerChoice;
     playerChoice = getUserChoice();
     computerChoice = getComputerChoice();
@@ -18,64 +19,90 @@ int main(){
     return 0;
 }
 
-char getUserChoice(){
+char getUserChoice()
+{
 
     char choice;
     std::cout << "Enter your choice (R for Rock, P for Paper, S for Scissors): ";
     std::cin >> choice;
-    return choice;  
+    return choice;
 }
 
-char getComputerChoice(){
-    
+char getComputerChoice()
+{
+
     srand(time(0));
-    
+
     int num = rand() % 3;
-    
-    if(num == 0){
+
+    if (num == 0)
+    {
         return 'R';
     }
-    
-    else if(num == 1){
+
+    else if (num == 1)
+    {
         return 'P';
-    } 
-    
-    else {
+    }
+
+    else
+    {
         return 'S';
     }
     return 'R'; // Default return value
 }
 
-void showchoice(char choice){
+void showchoice(char choice)
+{
 
-    if(choice == 'R'){
+    if (choice == 'R')
+    {
         std::cout << "Rock" << '\n';
-    } 
-    
-    else if(choice == 'P'){
+        return;
+    }
+
+    else if (choice == 'P')
+    {
         std::cout << "Paper" << '\n';
-    } 
-    
-    else if(choice == 'S'){
+        return;
+    }
+
+    else if (choice == 'S')
+    {
         std::cout << "Scissors" << '\n';
-    } 
-    
-    else {
+        return;
+    }
+
+    else
+    {
         std::cout << "Invalid choice" << '\n';
+        return;
     }
 }
 
-void chooseWinner(char player , char computer){
-    
-    if(player == computer){
+void chooseWinner(char player, char computer)
+{
+
+    if (player == computer)
+    {
         std::cout << "It's a tie!!" << '\n';
+        return;
     }
 
-    else if((player == 'R' && computer == 'S') || (player == 'P' && computer == 'R') || (player == 'S' && computer == 'P')){
+    else if ((player == 'R' && computer == 'S') || (player == 'P' && computer == 'R') || (player == 'S' && computer == 'P'))
+    {
         std::cout << "You win!!" << '\n';
-    } 
-    
-    else {
+        return;
+    }
+
+    else if ((player == 'S' && computer == 'R') || (player == 'R' && computer == 'P') || (player == 'P' && computer == 'S'))
+    {
         std::cout << "Computer wins!!" << '\n';
+        return;
+    }
+    else
+    {
+        std::cout << "try again" << '\n';
+        return;
     }
 }
