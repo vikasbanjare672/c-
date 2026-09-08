0e7fa594d39d5619a68483af894085b5d42ede05
Vikas Banjare <vikasbanjare6721@gmail.com>
2026-09-08 22:47:48 +0530

    Create bubble_sorting.cpp

---
+++ Newdevice_progfiles/bubble_sorting.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+void sort(int array[] , int size);
+
+int main(){
+    int array [] = { 1, 3, 5, 6, 2 , 7 , 9 ,10 ,8 , 4};
+    int size = sizeof(array)/sizeof(array[0]);
+
+    sort(array , size);
+
+    for ( int element : array){
+        std::cout << element << " ";
+    }
+    
+    return 0;
+}
+
+void sort(int array[] , int size){
+    
+    int temp;
+
+    for(int i = 0 ; i < size - 1 ; i++){
+        for(int j = 0 ; j < size - i - 1 ; j++){
+            if(array[j] > array[j + 1]){
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+
