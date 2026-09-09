e840ea1b68e9f40dd8c2870810ccce7ae1dc3358
Vikas Banjare <vikasbanjare6721@gmail.com>
2026-09-09 08:03:07 +0530

    Update Quiz_Game.cpp

--- Projects/Quiz_Game.cpp
+++ Projects/Quiz_Game.cpp
@@ -2,9 +2,9 @@
 
 int main(){
 
-    std::string questions[] = {"1. In which year C++ is developed ??"
-                               "2. Who invented C++ ?"
-                               "3. What is the predecessor of C++ ??"
+    std::string questions[] = {"1. In which year C++ is developed ??",
+                               "2. Who invented C++ ?",
+                               "3. What is the predecessor of C++ ??",
                                "4. Is your gf loyal ?"};
 
     std::string options[][4] = {{"A. 1963   B. 1982   C. 1800   D. 1984"},
@@ -12,7 +12,7 @@ int main(){
                                 {"A. B   B. C+   C. C   D. Assembly"},
                                 {"A. NO!!!   B. yes   C. Maybe   D. All of the above"}};
 
-    std::string anskey[] = {"D" , "D" , "C" , "B"};
+    char anskey[] = {'D' , 'D' , 'C' , 'B'};
 
     int size = sizeof(questions)/sizeof(questions[0]);
     char guess;
@@ -25,9 +25,24 @@ int main(){
         std::cout << "********************************\n";
 
         for(int j = 0 ; j < sizeof(options[i])/sizeof(options[i][0]) ; j++){
-            std::cout << options[i][j] << " \n";
-
+            std::cout << options[i][j] << " ";
         }
+        std::cout <<  '\n';
+        std::cout << "Your Answer : ";
+        std::cin >> guess ;
+        guess = toupper(guess);
+
+        if(guess == anskey[i]){
+            std::cout << "Correct " << '\n';
+            score++;
+        }
+        else{
+            std::cout << "Wrong!!" << '\n';
+           std::cout << "Correct ans : " << anskey[i]; 
+        }
+        std::cout << "Correct Answers : " << score << '\n';
+        std::cout << "Your Score : " << (score/double(size))*100 << " % " << "Correct!" << '\n';
+
     }
 
     return 0;
