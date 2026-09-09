b416953939285cf70dd5311a92ec08d8bfef1947
Vikas Banjare <vikasbanjare6721@gmail.com>
2026-09-09 23:42:32 +0530

    Create Creditcard__number_is_valid_or_not_program.cpp

---
+++ Projects/Creditcard__number_is_valid_or_not_program.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+// Luhn Algorithm :: 
+
+// 1. Double every second digit from right to left If doubled number is 2 digits, split them
+// 2. Add all single digits from step 1
+// 3. Add all odd numbered digits from right to left
+// 4. Sum results from steps 2 & 3
+// 5. If step 4 is divisble by 10, # is valid
+
+
+
+int main(){
+
+
+
+    return 0;
+}
