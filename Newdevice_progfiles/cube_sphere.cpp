30d7e1d26bcf8d372d0c3c2a43a275bebf8376ce
Vikas Banjare <vikasbanjare6721@gmail.com>
2026-09-13 23:59:54 +0530

    Create cube_sphere.cpp

---
+++ Newdevice_progfiles/cube_sphere.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+class shape{
+    public:
+        double area;
+        double volume;
+};
+
+class Cube : public shape{
+    public:
+        double side;
+    Cube(double side){
+        this->side = side;
+        this->area = side * side * 6;
+        this->volume = side * side * side;
+    }
+};
+
+class Sphere : public shape{
+    public:
+        double radius;
+    Sphere(double radius){
+        this->radius = radius;
+        this->area = 4 * 3.14159 * (radius * radius);
+        this->volume = (4/3.0) * 3.14159 * (radius * radius* radius);
+    }
+};
+
+int main(){
+int main(){
+
+    Cube cube(10);
+    Sphere sphere(7);
+
+    std::cout << "Area: " << cube.area << "cm\n";
+    std::cout << "Volume: " << cube.volume << "cm\n";
+
+    std::cout << "Area: " << sphere.area << "cm\n";
+    std::cout << "Volume: " << sphere.volume << "cm\n";
+
+    return 0;
+}
+}
